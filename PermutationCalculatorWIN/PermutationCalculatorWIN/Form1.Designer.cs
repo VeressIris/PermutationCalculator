3f6b50abb54f6aa694986416e3353ac290fb49df
VeressIris <irisveress@gmail.com>
2023-10-09 16:58:15 +0300

    rough version
    basic logic added, garbage UI

--- PermutationCalculatorWIN/PermutationCalculatorWIN/Form1.Designer.cs
+++ PermutationCalculatorWIN/PermutationCalculatorWIN/Form1.Designer.cs
@@ -28,13 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.calculator_bttn = new System.Windows.Forms.Button();
+            this.perm1_txtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.perm2_txtBox = new System.Windows.Forms.TextBox();
+            this.leftBrackets = new System.Windows.Forms.Label();
+            this.leftBrackets2 = new System.Windows.Forms.Label();
+            this.rightBrackets2 = new System.Windows.Forms.Label();
+            this.rightBrackets = new System.Windows.Forms.Label();
+            this.resultLetter_lbl = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.n_txtBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.result_lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculator_bttn
+            // 
+            this.calculator_bttn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.calculator_bttn.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.calculator_bttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculator_bttn.Location = new System.Drawing.Point(501, 229);
+            this.calculator_bttn.Name = "calculator_bttn";
+            this.calculator_bttn.Size = new System.Drawing.Size(118, 49);
+            this.calculator_bttn.TabIndex = 1;
+            this.calculator_bttn.Text = "calculate";
+            this.calculator_bttn.UseVisualStyleBackColor = false;
+            this.calculator_bttn.Click += new System.EventHandler(this.calculator_bttn_Click);
+            // 
+            // perm1_txtBox
+            // 
+            this.perm1_txtBox.AcceptsReturn = true;
+            this.perm1_txtBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.perm1_txtBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.perm1_txtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perm1_txtBox.Location = new System.Drawing.Point(98, 71);
+            this.perm1_txtBox.Multiline = true;
+            this.perm1_txtBox.Name = "perm1_txtBox";
+            this.perm1_txtBox.Size = new System.Drawing.Size(112, 85);
+            this.perm1_txtBox.TabIndex = 2;
+            this.perm1_txtBox.Text = "1 2 3 4 5\r\n3 4 1 2 5";
+            this.perm1_txtBox.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "α =";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(417, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "β =";
+            // 
+            // perm2_txtBox
+            // 
+            this.perm2_txtBox.AcceptsReturn = true;
+            this.perm2_txtBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.perm2_txtBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.perm2_txtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.perm2_txtBox.Location = new System.Drawing.Point(501, 73);
+            this.perm2_txtBox.Multiline = true;
+            this.perm2_txtBox.Name = "perm2_txtBox";
+            this.perm2_txtBox.Size = new System.Drawing.Size(118, 85);
+            this.perm2_txtBox.TabIndex = 2;
+            this.perm2_txtBox.Text = "1 2 3 4 5\r\n2 5 4 3 1";
+            this.perm2_txtBox.WordWrap = false;
+            // 
+            // leftBrackets
+            // 
+            this.leftBrackets.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.leftBrackets.AutoSize = true;
+            this.leftBrackets.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftBrackets.Location = new System.Drawing.Point(60, 53);
+            this.leftBrackets.Name = "leftBrackets";
+            this.leftBrackets.Size = new System.Drawing.Size(36, 110);
+            this.leftBrackets.TabIndex = 4;
+            this.leftBrackets.Text = "⎡\r\n⎣";
+            // 
+            // leftBrackets2
+            // 
+            this.leftBrackets2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.leftBrackets2.AutoSize = true;
+            this.leftBrackets2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftBrackets2.Location = new System.Drawing.Point(465, 53);
+            this.leftBrackets2.Name = "leftBrackets2";
+            this.leftBrackets2.Size = new System.Drawing.Size(36, 110);
+            this.leftBrackets2.TabIndex = 4;
+            this.leftBrackets2.Text = "⎡\r\n⎣";
+            // 
+            // rightBrackets2
+            // 
+            this.rightBrackets2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rightBrackets2.AutoSize = true;
+            this.rightBrackets2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightBrackets2.Location = new System.Drawing.Point(611, 55);
+            this.rightBrackets2.Name = "rightBrackets2";
+            this.rightBrackets2.Size = new System.Drawing.Size(36, 110);
+            this.rightBrackets2.TabIndex = 4;
+            this.rightBrackets2.Text = "⎤\r\n⎦\r\n";
+            // 
+            // rightBrackets
+            // 
+            this.rightBrackets.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rightBrackets.AutoSize = true;
+            this.rightBrackets.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightBrackets.Location = new System.Drawing.Point(208, 55);
+            this.rightBrackets.Name = "rightBrackets";
+            this.rightBrackets.Size = new System.Drawing.Size(36, 110);
+            this.rightBrackets.TabIndex = 4;
+            this.rightBrackets.Text = "⎤\r\n⎦\r\n";
+            // 
+            // resultLetter_lbl
+            // 
+            this.resultLetter_lbl.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.resultLetter_lbl.AutoSize = true;
+            this.resultLetter_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLetter_lbl.Location = new System.Drawing.Point(14, 219);
+            this.resultLetter_lbl.Name = "resultLetter_lbl";
+            this.resultLetter_lbl.Size = new System.Drawing.Size(66, 39);
+            this.resultLetter_lbl.TabIndex = 3;
+            this.resultLetter_lbl.Text = "σ =";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(60, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 110);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "⎡\r\n⎣";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(196, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 110);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "⎤\r\n⎦\r\n";
+            // 
+            // n_txtBox
+            // 
+            this.n_txtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n_txtBox.Location = new System.Drawing.Point(300, 12);
+            this.n_txtBox.Name = "n_txtBox";
+            this.n_txtBox.Size = new System.Drawing.Size(100, 44);
+            this.n_txtBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(220, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "n = ";
+            // 
+            // result_lbl
+            // 
+            this.result_lbl.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.result_lbl.AutoSize = true;
+            this.result_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result_lbl.Location = new System.Drawing.Point(85, 219);
+            this.result_lbl.Name = "result_lbl";
+            this.result_lbl.Size = new System.Drawing.Size(164, 31);
+            this.result_lbl.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.result_lbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.n_txtBox);
+            this.Controls.Add(this.rightBrackets);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rightBrackets2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.leftBrackets2);
+            this.Controls.Add(this.leftBrackets);
+            this.Controls.Add(this.resultLetter_lbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.perm2_txtBox);
+            this.Controls.Add(this.perm1_txtBox);
+            this.Controls.Add(this.calculator_bttn);
+            this.Name = "Form1";
+            this.Text = "Permutation Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button calculator_bttn;
+        private System.Windows.Forms.TextBox perm1_txtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox perm2_txtBox;
+        private System.Windows.Forms.Label leftBrackets;
+        private System.Windows.Forms.Label leftBrackets2;
+        private System.Windows.Forms.Label rightBrackets2;
+        private System.Windows.Forms.Label rightBrackets;
+        private System.Windows.Forms.Label resultLetter_lbl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox n_txtBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label result_lbl;
     }
 }
 
--- PermutationCalculatorWIN/PermutationCalculatorWIN/Form1.cs
+++ PermutationCalculatorWIN/PermutationCalculatorWIN/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,87 @@ namespace PermutationCalculatorWIN
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void calculator_bttn_Click(object sender, EventArgs e)
+        {
+            //get data:
+            int n = int.Parse(n_txtBox.Text);
+            int[,] a = new int[2, n];
+            int[,] b = new int[2, n];
+            int[,] result = new int[2, n];
+            GetPermutation(a, perm1_txtBox.Text, n);
+            GetPermutation(b, perm2_txtBox.Text, n);
+            
+            Multiply(a, b, n, result);
+            OutputPerm(result, n);
+        }
+
+        private void Multiply(int[,] a, int[,] b, int n, int[,] result)
+        {
+            GenerateFirstLine(result, n);
+
+            for (int index = 0; index < n; index++)
+            {
+                //i had no clue how else to name these variables. very unfortunate naming...
+                int step1 = b[1, index];
+                int step2 = a[1, step1 - 1];
+                result[1, index] = step2;
+            }
+        }
+
+        private void GetPermutation(int[,] perm, string input, int n)
+        {
+            GenerateFirstLine(perm, n);
+            
+            int newLineIndex = input.IndexOf('\n');
+            if (newLineIndex != -1)
+            {
+                int j = 0;
+                for (int k = newLineIndex + 1; k < input.Length; k+=2)
+                {
+                    perm[1, j] = input[k] - '0';
+                    j++;
+                }
+            }
+        }
+
+        private void GenerateFirstLine(int[,] perm, int n)
+        {
+            for (int i = 0; i < n; i++)
+                perm[0, i] = i + 1;
+        }
+
+        private string GenerateFirstLine(int n)
+        {
+            string str = string.Empty;
+            for (int i = 0; i < n; i++)
+                str += i + 1 + " ";
+            str += "\n";
+
+            return str;
+        }
+
+        private void OutputPerm(int[,] result, int n)
+        {
+            result_lbl.Text = GenerateFirstLine(n);
+            for (int i = 0; i < n; i++)
+            {
+                result_lbl.Text += result[1, i] + " ";
+            }
+        }
+
+        //!can get rid of later!
+        private void printPerm(int[,] perm)
+        {
+            for (int i = 0; i <  perm.GetLength(0); i++)
+            {
+                for (int j = 0; j < perm.GetLength(1); j++)
+                {
+                    Debug.Write(perm[i, j] + " ");
+                }
+                Debug.Write("\n");
+            }
         }
     }
 }
